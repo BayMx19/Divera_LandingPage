41e33944349a4eb402d8779621b752362072e2a1
BayMx19 <isaimanmuhammad1@gmail.com>
2025-04-10 22:57:43 +0700

    Update Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,158 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="fables-transparent py-3 py-lg-0">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12">
+            <nav class="navbar navbar-expand-md btco-hover-menu py-lg-2">
+              <!-- Brand di kiri -->
+              <a class="navbar-brand fables-logo-brand pl-0" href="/">
+                <div class="logo-nav">Divera Tech.</div>
+              </a>
+
+              <!-- Hamburger button untuk mobile -->
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#fablesNavDropdown"
+                aria-controls="fablesNavDropdown"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span class="fables-iconmenu-icon text-white font-16"></span>
+              </button>
+
+              <!-- Menu di kanan -->
+              <div class="collapse navbar-collapse justify-content-end" id="fablesNavDropdown">
+                <ul class="navbar-nav fables-nav">
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="/" id="sub-nav1"> Beranda </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="/" id="sub-nav1"> Tentang Kami </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="/" id="sub-nav1"> Layanan Kami </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" href="/" id="sub-nav1"> Portofolio </a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link" style="text-decoration: underline" href="/" id="sub-nav1">
+                      <b>Hubungi Kami</b>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Start Header -->
+    <div
+      class="fables-header fables-after-overlay overlay-lighter index-traingle bg-rules"
+      style="background-image: url(assets/custom/images/index-background.jpg)"
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-lg-7 mr-auto index-carousel">
+            <div
+              class="owl-carousel owl-theme default-carousel nav-0 z-index mt-md-4 mt-xl-5 pt-md-4 pt-xl-5 dots-0 pb-md-5"
+            >
+              <div
+                class="pt-0 mt-0 pt-xl-5 mt-xl-5 wow slideInUp"
+                data-wow-duration="2s"
+                data-wow-delay=".4s"
+              >
+                <h1 class="fables-main-text-color font-weight-bold mb-1">
+                  CONSULTING SERVICE FOR ALL
+                  <span class="fables-second-text-color">KIND OF BUSINESSES</span>
+                </h1>
+                <p class="fables-forth-text-colo mb-3 light-font fables-header-slider-details">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                  Ipsum has been the industry's standard dummy text ever since the 1500s,
+                </p>
+                <a
+                  href="#"
+                  class="btn fables-second-background-color fables-second-border-color white-color rounded-0 mr-4 px-3 px-md-4 py-2 bg-hover-transparent fables-second-hover-color"
+                  >Our Services</a
+                >
+                <a
+                  href="#"
+                  class="btn fables-second-border-color fables-second-text-color rounded-0 px-3 px-md-4 py-2 fables-second-hover-background-color"
+                  >Learn More</a
+                >
+              </div>
+              <div
+                class="pt-0 mt-0 pt-xl-5 mt-xl-5 wow slideInUp"
+                data-wow-duration="2s"
+                data-wow-delay=".8s"
+              >
+                <h1 class="fables-main-text-color font-weight-bold mb-1">
+                  CONSULTING SERVICE FOR ALL
+                  <span class="fables-second-text-color">KIND OF BUSINESSES</span>
+                </h1>
+                <p class="fables-forth-text-colo mb-3 light-font fables-header-slider-details">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                  Ipsum has been the industry's standard dummy text ever since the 1500s,
+                </p>
+                <a
+                  href="#"
+                  class="btn fables-second-background-color fables-second-border-color white-color rounded-0 mr-4 px-3 px-md-4 py-2 bg-hover-transparent fables-second-hover-color"
+                  >Our Services</a
+                >
+                <a
+                  href="#"
+                  class="btn fables-second-border-color fables-second-text-color rounded-0 px-3 px-md-4 py-2 fables-second-hover-background-color"
+                  >Learn More</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /End Header -->
+    <!-- section data trend -->
+    <div class="row">
+      <div class="col-sm-12"></div>
+    </div>
+    <!-- endsection data trend -->
+
+    <!-- section why choose us -->
+    <div class="row">
+      <div class="col-sm-12"></div>
+    </div>
+    <!-- endsection why choose us -->
+    <!-- section our services -->
+    <div class="row">
+      <div class="col-sm-12"></div>
+    </div>
+    <!-- endsection our services -->
+    <!-- section portfolio -->
+    <div class="row">
+      <div class="col-sm-12"></div>
+    </div>
+    <!-- endsection portfolio -->
+    <!-- section kata2 iklan -->
+    <div class="row">
+      <div class="col-sm-12"></div>
+    </div>
+    <!-- endsection kata2 iklan -->
+    <!-- section contact us -->
+    <div class="row">
+      <div class="col-sm-12"></div>
+    </div>
+    <!-- endsection contact us -->
+    <!-- section footer -->
+    <div class="row">
+      <div class="col-sm-12"></div>
+    </div>
+    <!-- endsection footer -->
   </main>
 </template>
