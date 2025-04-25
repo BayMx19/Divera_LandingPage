dd8182d2cdc1883d01cd03b3a0fa6ffd6172b760
BayMx19 <isaimanmuhammad1@gmail.com>
2025-04-25 09:56:16 +0700

    Edit Footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -521,38 +521,6 @@
     <footer id="newsletter">
       <div class="container">
         <div class="row">
-          <div class="col-lg-8 offset-lg-2">
-            <div class="section-heading">
-              <h4>Join our mailing list to receive the news &amp; latest trends</h4>
-            </div>
-          </div>
-          <div class="col-lg-6 offset-lg-3">
-            <form id="search" action="#" method="GET">
-              <div class="row">
-                <div class="col-lg-6 col-sm-6">
-                  <fieldset>
-                    <input
-                      type="address"
-                      name="address"
-                      class="email"
-                      placeholder="Email Address..."
-                      autocomplete="on"
-                      required
-                    />
-                  </fieldset>
-                </div>
-                <div class="col-lg-6 col-sm-6">
-                  <fieldset>
-                    <button type="submit" class="main-button">
-                      Subscribe Now <i class="fa fa-angle-right"></i>
-                    </button>
-                  </fieldset>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-        <div class="row">
           <div class="col-lg-3">
             <div class="footer-widget">
               <h4>Contact Us</h4>
@@ -601,20 +569,12 @@
               <div class="logo">
                 <img src="/assets/images/white-logo.png" alt="" />
               </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore.
-              </p>
+              <p>Build Smarter. Launch Faster. With <b class="text-bold">DiveraTech</b></p>
             </div>
           </div>
           <div class="col-lg-12">
             <div class="copyright-text">
-              <p>
-                Copyright © 2022 Chain App Dev Company. All Rights Reserved. <br />Design:
-                <a href="https://templatemo.com/" target="_blank" title="css templates"
-                  >TemplateMo</a
-                >
-              </p>
+              <p>Copyright © 2025 DiveraTech. All Rights Reserved. <br /></p>
             </div>
           </div>
         </div>
